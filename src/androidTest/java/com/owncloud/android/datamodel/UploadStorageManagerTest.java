90fdc6dddf3125d239cf4e2bf4d5eed2cfa1380b
tobiasKaminsky <tobias@kaminsky.me>
2020-04-23 08:26:58 +0200

    Add some tests for upload storage manager
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/androidTest/java/com/owncloud/android/datamodel/UploadStorageManagerTest.java
+++ src/androidTest/java/com/owncloud/android/datamodel/UploadStorageManagerTest.java
@@ -6,18 +6,25 @@ import android.content.Context;
 
 import com.nextcloud.client.account.CurrentAccountProvider;
 import com.owncloud.android.db.OCUpload;
+import com.owncloud.android.db.UploadResult;
+import com.owncloud.android.files.services.FileUploader;
+import com.owncloud.android.operations.UploadFileOperation;
+
+import net.bytebuddy.utility.RandomString;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.File;
+import java.util.Random;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by JARP on 6/7/17.
@@ -50,20 +57,57 @@ public class UploadStorageManagerTest {
         insertUploads(Accounts[0], accountRowsA);
         insertUploads(Accounts[1], accountRowsB);
 
-        Assert.assertTrue("Expected 4 removed uploads files", uploadsStorageManager.removeAccountUploads(Accounts[1]) == 4);
+        assertEquals("Expected 4 removed uploads files",
+                     4,
+                     uploadsStorageManager.removeAccountUploads(Accounts[1]));
+    }
+
+    @Test
+    public void largeTest() {
+        int size = 1000;
+
+        insertUploads(Accounts[0], size);
+
+        assertEquals(size + 1, uploadsStorageManager.getAllStoredUploads().length);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void corruptedUpload() {
+        OCUpload corruptUpload = new OCUpload(File.separator + "LocalPath",
+                                              OCFile.PATH_SEPARATOR + "RemotePath",
+                                              Accounts[0].name);
+
+        corruptUpload.setLocalPath(null);
+
+        uploadsStorageManager.storeUpload(corruptUpload);
+
+        uploadsStorageManager.getAllStoredUploads();
     }
 
     private void insertUploads(Account account, int rowsToInsert) {
-
         for (int i = 0; i < rowsToInsert; i++) {
             uploadsStorageManager.storeUpload(createUpload(account));
         }
     }
 
-    private OCUpload createUpload(Account acc) {
-        return new OCUpload(File.separator + "LocalPath",
-                OCFile.PATH_SEPARATOR + "RemotePath",
-                acc.name);
+    private OCUpload createUpload(Account account) {
+        OCUpload upload = new OCUpload(File.separator + "LocalPath",
+                                       OCFile.PATH_SEPARATOR + "RemotePath",
+                                       account.name);
+
+        upload.setFileSize(new Random().nextInt(20000));
+        upload.setUploadStatus(UploadsStorageManager.UploadStatus.UPLOAD_IN_PROGRESS);
+        upload.setLocalAction(2);
+        upload.setNameCollisionPolicy(FileUploader.NameCollisionPolicy.ASK_USER);
+        upload.setCreateRemoteFolder(false);
+        upload.setUploadEndTimestamp(System.currentTimeMillis());
+        upload.setLastResult(UploadResult.DELAYED_FOR_WIFI);
+        upload.setCreatedBy(UploadFileOperation.CREATED_BY_USER);
+        upload.setUseWifiOnly(true);
+        upload.setWhileChargingOnly(false);
+        upload.setFolderUnlockToken(RandomString.make(10));
+
+        return upload;
     }
 
     @After
